d4f5230fa242bd20cb2697d5474812b78ebc5835
Taehun Kim <bigteach0508@pusan.ac.kr>
2024-05-07 21:21:55 +0900

    create hw2

---
+++ HW2/sample_server.js
@@ -0,0 +1,14 @@
+const {createServer} = require('node:http')
+
+const hostname='0.0.0.0';
+const port=3000;
+
+const server=createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    res.end(`hostname : ${hostname}, port : ${port}`);
+})
+
+server.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
